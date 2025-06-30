0c564846e9388668dc5ec8be7cf28155bd757de1
ParthivP7 <imshasha1@gmail.com>
2025-06-30 11:32:53 -0400

    🧱 Configure EF Core DbContext and entity mapping

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Domain.VideoGames;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<VideoGame> VideoGames { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
